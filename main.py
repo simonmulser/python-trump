8c0745ba4e38ccb132118eb09168702bfd1da387
Simon Mulser <simonmulser@gmail.com>
2017-07-20 12:46:44 +0200

    request always full tweet

--- main.py
+++ main.py
@@ -13,12 +13,13 @@ newest = 850800045012201473
 api = twitter.Api(consumer_key=credentials.consumer_key,
 					consumer_secret=credentials.consumer_secret,
 					access_token_key=credentials.access_token_key,
-					access_token_secret=credentials.access_token_secret)
+					access_token_secret=credentials.access_token_secret,
+					tweet_mode='extended')
 
 
 with open('tweets.csv', 'w') as file:
 	writer = csv.writer(file, delimiter='|')
-	writer.writerow(['created_at', 'text', 'text_truncated', 'source', 'retweet_count', 'favorite_count', 'link'])
+	writer.writerow(['created_at', 'text', 'source', 'retweet_count', 'favorite_count', 'link'])
 
 with open('tweets.csv', 'a') as file:
 	writer = csv.writer(file, delimiter='|')
@@ -28,5 +29,5 @@ with open('tweets.csv', 'a') as file:
 		tweets = api.GetUserTimeline(user_id=25073877, since_id=796797436752707585 - 1, max_id=current, count=200)
 		print('len tweets {}'.format(len(tweets)))
 		for tweet in tweets:
-			writer.writerow([tweet.created_at, tweet.text, tweet.truncated, tweet.source, tweet.retweet_count, tweet.favorite_count, 'https://twitter.com/realdonaldtrump/status/{}'.format(tweet.id)])
+			writer.writerow([tweet.created_at, tweet.full_text, tweet.source, tweet.retweet_count, tweet.favorite_count, 'https://twitter.com/realdonaldtrump/status/{}'.format(tweet.id)])
 			current = tweet.id
